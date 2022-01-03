b7b35ed93e43888f00887fae85a0c72e8a498ec8
fingann <fingann92@gmail.com>
2022-01-03 02:00:24 +0100

    Added Action.go
    This is used for functions mimicing an action

---
+++ internal/Action.go
@@ -0,0 +1,3 @@
+package internal
+
+type Action[T any] func(object T)
--- main.go
+++ main.go
@@ -7,16 +7,20 @@ import (
 )
 
 func main() {
-	lst2 := Collections.NewList([]string{"!"})
+	// Create a new List and retrieve values
 	lst := Collections.NewList([]string{"hello,", " world"})
-	lst.AddRange(lst2)
 	val, _ := lst.Get(0)
 	fmt.Print(val)
 	val, _ = lst.Get(1)
 	fmt.Print(val)
+
+	// Add ranges to list
+	lst2 := Collections.NewList([]string{"!", "!"})
+	lst.AddRange(lst2)
 	val, _ = lst.Get(2)
 	fmt.Println(val)
 
+	// Search for values within lists using predicate
 	item, err := lst.Find(func(needle string) bool {
 		return needle == "hello,"
 	})
